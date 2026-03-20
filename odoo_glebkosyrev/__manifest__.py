03e0108f803edf4261ffd093190a439321db3a4a
Gleb Kosyrev <fakemirror11@gmail.com>
2026-03-20 15:50:20 +0300

    chore: downgrade to odoo 18

--- odoo_glebkosyrev/__manifest__.py
+++ odoo_glebkosyrev/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'odoo_glebkosyrev',
-    'version': '19.0.1.0.0',
+    'version': '18.0.1.0.0',
     'summary': 'Extends Sale Order with new fields',
     'author': 'Gleb Kosyrev',
     'category': 'Sales',
